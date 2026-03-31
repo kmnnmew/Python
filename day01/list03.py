96876a3ed06286dd2fdc164369f6acf76c863339
kmnnmew <shimhayun08@gmail.com>
2026-03-31 20:14:48 +0900

    list03

---
+++ day01/list03.py
@@ -0,0 +1,19 @@
+word = "racecar"
+for i in range(len(word)//2+1): 
+  if word[i] == word[len(word)-1-i]: 
+    result = True
+  else:
+    result = False
+
+print(result)
+
+words = ["racecar", "hello", "level", "python", "madam", "world"]
+for j in range(len(words)):
+  for i in range(len(words[j])//2+1): 
+    if words[j][i] == words[j][len(words[j])-1-i]: 
+      result = True
+    else:
+      result = False
+      break
+  if result == True:
+    print(words[j])
