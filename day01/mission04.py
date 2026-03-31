4e1cdab848ba63f5f03634528ce05a3c695276b4
kmnnmew <shimhayun08@gmail.com>
2026-03-31 19:17:02 +0900

    mission05 clear

--- day01/mission04.py
+++ day01/mission04.py
@@ -1,17 +1,23 @@
 import random
-num = random.randint(1,100)
 success = False
-count = 0
-print(num)
-while(not success):
-  guess = int(input("숫자를 맞춰보세요 (1~100): "))
-  if num == guess:
-    count += 1
-    print(f"정답! {count}번 만에 맞췄어요!")
-    success = True;
-  elif num > guess:
-    count += 1
-    print("더 높아요!")
-  elif num < guess:
-    count += 1
-    print("더 낮아요!")
+game = True
+while game: 
+  num = random.randint(1,100)
+  print(num)
+  count = 0
+  while not success:
+    guess = int(input("숫자를 맞춰보세요 (1~100): "))
+    if num == guess:
+      count += 1
+      print(f"정답! {count}번 만에 맞췄어요!")
+      success = True;
+    elif num > guess:
+      count += 1
+      print("더 높아요!")
+    elif num < guess:
+      count += 1
+      print("더 낮아요!")
+  end = input("다시 할까요? (y/n) ")
+  if end == "y":
+    success = False
+  else: game = False
---
+++ day01/mission05.py
@@ -0,0 +1,46 @@
+def add():
+  a = int(input("첫 번째 숫자: "));
+  b = int(input("두 번째 숫자: "));
+  print(f"결과: {a} + {b} = {a+b}")
+  return a+b
+
+def min():
+  a = int(input("첫 번째 숫자: "));
+  b = int(input("두 번째 숫자: "));
+  print(f"결과: {a} - {b} = {a-b}")
+  return a-b
+
+def mul():
+  a = int(input("첫 번째 숫자: "));
+  b = int(input("두 번째 숫자: "));
+  print(f"결과: {a} * {b} = {a*b}")
+  return a*b
+
+def div():
+  a = int(input("첫 번째 숫자: "));
+  b = int(input("두 번째 숫자: "));
+  if b == 0: 
+    print("error!!! 잘못된 숫자입니다!!")
+    return "error"
+  print(f"결과: {a} / {b} = {a/b}")
+  return a/b
+
+list1 = [];
+
+for i in range(1, 6): 
+  print("== 계산기 ==")
+  print("1. 더하기  2. 빼기")
+  print("3. 곱하기  4. 나누기  5. 종료")
+  print()
+  choice = int(input("선택: "))
+  if choice == 1:
+    list1.append(add())
+  elif choice == 2:
+    list1.append(min())
+  elif choice == 3:
+    list1.append(mul())
+  elif choice == 4:
+    list1.append(div())
+  else: break
+
+print(list1)
