7334a1372a25587954062c49dbc77b72476232b8
kmnnmew <shimhayun08@gmail.com>
2026-03-31 20:31:36 +0900

    list03 add list slice version

--- day01/list03.py
+++ day01/list03.py
@@ -7,6 +7,12 @@ for i in range(len(word)//2+1):
 
 print(result)
 
+if word == word[::-1]:
+  result = True
+else:
+  result = False
+print(result)
+
 words = ["racecar", "hello", "level", "python", "madam", "world"]
 for j in range(len(words)):
   for i in range(len(words[j])//2+1): 
@@ -17,3 +23,7 @@ for j in range(len(words)):
       break
   if result == True:
     print(words[j])
+
+for w in words:
+  if w == w[::-1]:
+    print(w)
