2b34aa9bcb6eb7a1e4f91f9a25ff27d75d5d3686
kmnnmew <shimhayun08@gmail.com>
2026-04-03 19:28:47 +0900

    dictionary02 clear

---
+++ day02/dictionary02.py
@@ -0,0 +1,19 @@
+sentence = "apple banana apple orange banana apple"
+
+sentence = sentence.split(' ');
+
+count = {};
+
+for word in sentence: 
+  count[word] = count.get(word, 0) + 1
+
+print(count)
+
+compareKey = ''
+compareValue = 0
+for key, value in count.items(): 
+  if value >= compareValue: 
+    compareValue = value
+    compareKey = key
+
+print(f"가장 많이 등장한 단어: {compareKey}");
