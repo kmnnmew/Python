0f74370df7fc72b567c8e1a0e4e6d359f30d07a1
kmnnmew <shimhayun08@gmail.com>
2026-04-04 22:49:06 +0900

    1000, 2744, 10798 clear

---
+++ day03/beakjoon.py
@@ -0,0 +1,34 @@
+# num = input();
+# a = int(num.split(' ')[0])
+# b = int(num.split(' ')[1])
+# print(f"{a + b}")
+
+# sentence  = input();
+# result = ''
+
+# for ch in sentence: 
+#   if ch.isupper(): 
+#     result += ch.lower()
+#   else: 
+#     result += ch.upper()
+
+# print(result)
+
+# line1 = input()
+# line2 = input()
+# line3 = input()
+# line4 = input()
+# line5 = input()
+
+# lines = [line1, line2, line3, line4, line5]
+# max_len = max(len(line) for line in lines)
+
+# result = ''
+
+# for i in range(max_len):
+#     for line in lines:
+#         if i < len(line):
+#             result += line[i]
+
+# print(result)
+
