a609228cae6684922a7be9631ef7dddef366cb00
kmnnmew <shimhayun08@gmail.com>
2026-04-06 20:27:34 +0900

    9012 clear?

--- day05/beakjoon.py
+++ day05/beakjoon.py
@@ -1,22 +1,37 @@
-# nums = []
-# for i in range(10):
-#   nums.append(int(input()))
+# # nums = []
+# # for i in range(10):
+# #   nums.append(int(input()))
 
-# nums_remain = []
+# # nums_remain = []
 
-# for i in range(10): 
-#   nums_remain.append(int(nums[i]%42))
+# # for i in range(10): 
+# #   nums_remain.append(int(nums[i]%42))
 
-# print(len(set(nums_remain)))
+# # print(len(set(nums_remain)))
 
-num1, num2 = map(int, input().split())
+# num1, num2 = map(int, input().split())
 
-for i in range(num1, num2+1): 
-  if i < 2: 
-    continue
-  tf = True
-  for num in range(2, int(i**0.5)+1): 
-    if i % num == 0: 
-      tf = False
+# for i in range(num1, num2+1): 
+#   if i < 2: 
+#     continue
+#   tf = True
+#   for num in range(2, int(i**0.5)+1): 
+#     if i % num == 0: 
+#       tf = False
+#       break
+#   if tf: print(i)
+
+num = int(input())
+data = []
+for i in range(num): 
+  data.append(input())
+for i in range(num): 
+  count = 0
+  for ch in data[i]: 
+    if ch == '(':
+      count += 1
+    elif ch == ')':
+      count -= 1
+    if count < 0: 
       break
-  if tf: print(i)
+  print('YES' if count == 0 else 'NO')
--- day05/tempCodeRunnerFile.py
+++ day05/tempCodeRunnerFile.py
@@ -1,9 +1,11 @@
-num1, num2 = map(int, input().split())
-
-for i in range(num1, num2+1): 
-  tf = True
-  for num in range(2, int(i**0.5)+1): 
-    if i % num == 0: 
-      tf = False
-      break
-  if tf: print(i)
+num = int(input())
+data = []
+for i in range(num): 
+  data.append(input())
+for j in range(num): 
+  data2 = {}
+  for i in data[j]:
+    data2[i] = data2.get(i, 0)+1
+  if data2.get('(', 0) == data2.get(')', 0): 
+    print('YES')
+  else: print('NO')
