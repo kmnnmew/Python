3d49169c9b33500f57b1a55692494f1eb6099891
kmnnmew <shimhayun08@gmail.com>
2026-04-07 16:28:17 +0900

    11650 clear

--- day06/beakjoon.py
+++ day06/beakjoon.py
@@ -1,8 +1,19 @@
 # 1427
-num = input();
-nums = ""
-for i in range(len(num)): 
-  nums += sorted(num, reverse=True)[i]
-print(nums)
+# num = input();
+# nums = ""
+# for i in range(len(num)): 
+#   nums += sorted(num, reverse=True)[i]
+# print(nums)
 
 # 11650
+num = int(input())
+
+coor = []
+
+for _ in range(num): 
+  coor.append(list(map(int, input().split())))
+
+coor = sorted(coor, key=lambda k : (k[0], k[1]))
+
+for i in coor: 
+  print(*i)
