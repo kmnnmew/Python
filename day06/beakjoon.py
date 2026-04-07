02798c1c3b7bbddffa3640baf8e300863fe3be88
kmnnmew <shimhayun08@gmail.com>
2026-04-07 19:40:11 +0900

    1874 clear

--- day06/beakjoon.py
+++ day06/beakjoon.py
@@ -70,3 +70,43 @@
 #     if len(queue) > 0: 
 #       print(queue[-1])
 #     else: print(-1)
+
+# 1874
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+num = int(input()) # 최대 숫자 입력
+nums = deque() # 목표 저장
+nums2 = [] # 목표 확인
+incre = 0 # 최대 숫자까지 증가
+stack = [] # 증가하는 애가 저장
+result = [] # pop된 애가 저장
+result2 = [] # + -가 저장
+tf = True
+
+for _ in range(num):
+  input_data = int(input().strip())
+  nums.append(input_data)
+  nums2.append(input_data)
+
+while tf: 
+  if not stack: 
+    result2.append('+')
+    incre+=1
+    stack.append(incre)
+  if nums[0] != stack[-1]: 
+    result2.append('+')
+    incre+=1
+    stack.append(incre)
+  else:
+    nums.popleft()
+    result.append(stack.pop())
+    result2.append('-')
+  if nums2 == result:
+    tf = False
+    for i in result2:
+      print(i)
+  if incre > num: 
+    tf = False
+    print('NO')
