adfdcd0177bfcdd45f48fe4badcb9a97a100d98c
kmnnmew <shimhayun08@gmail.com>
2026-04-07 16:49:30 +0900

    11650 clear

--- day06/beakjoon.py
+++ day06/beakjoon.py
@@ -6,14 +6,38 @@
 # print(nums)
 
 # 11650
-num = int(input())
+# num = int(input())
 
-coor = []
+# coor = []
 
-for _ in range(num): 
-  coor.append(list(map(int, input().split())))
+# for _ in range(num): 
+#   coor.append(list(map(int, input().split())))
 
-coor = sorted(coor, key=lambda k : (k[0], k[1]))
+# coor = sorted(coor, key=lambda k : (k[0], k[1]))
 
-for i in coor: 
-  print(*i)
+# for i in coor: 
+#   print(*i)
+
+# 2581
+# num1 = int(input())
+# num2 = int(input())
+# count = []
+# total = 0
+
+# for i in range(num1, num2+1):
+#   if i < 2: 
+#     continue
+#   tf = True
+#   for num in range(2, int(i**0.5)+1): 
+#     if i%num == 0: 
+#       tf = False
+#       break
+#   if tf: count.append(i)
+
+# if len(count) > 0: 
+#   for i in count: 
+#     total += i
+#   print(total)
+#   print(min(count))
+# else: print(-1)
+
