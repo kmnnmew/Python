c27c1af9796f10fab324624383940f0dc55940ae
kmnnmew <shimhayun08@gmail.com>
2026-04-09 20:54:34 +0900

    돌게임 5,6 clear

---
+++ day07/beakjoon.py
@@ -0,0 +1,20 @@
+# # # 9658
+# # num = int(input())
+
+# # if num % 7 == 2 or num % 7 == 0: 
+# #   print("CY")
+# # else: print("SK")
+
+# # 
+# num = int(input())
+
+# if num % 2 == 0: 
+#   print("CY")
+# else: print("SK")
+
+# 9661
+num = int(input())
+
+if num % 7 == 1 or num % 7 == 4 or num % 7 == 2:
+  print("CY")
+else: print("SK")
---
+++ day07/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+num = int(input())
+
+if num % 2 == 0 or num % 7 == 0: 
+  print("CY")
