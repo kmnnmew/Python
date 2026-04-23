fcc89836c14b3c9b87483e88442139112058c463
kmnnmew <shimhayun08@gmail.com>
2026-04-23 18:57:52 +0900

    완주하지 못한 선수 클리어

---
+++ day08/test.py
@@ -0,0 +1,23 @@
+# # 완주하지 못한 선수
+# def solution(participant, completion):
+#     answer = ''
+#     participant.sort()
+#     completion.sort()
+#     print(participant)
+#     print(completion)
+#     for i in range(len(participant)):
+#         if i > len(completion)-1:
+#             completion.append(None)
+#         if participant[i] != completion[i]:
+#             answer = participant[i]
+#             break
+#     return answer
+
+# print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
+
+# 전화번호 목록
+def solution(phone_book):
+    answer = True
+    for i in phone_book:
+        
+    return answer
